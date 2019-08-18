b8eb0e6da90eed6aa3e3187de209ffc0e99a7663
Rahul Somasunderam <rahul.som@gmail.com>
2019-08-18 13:54:38 -0700

    Fix `native-image` installation in dockerized linux

--- .docker/install-graal.sh
+++ .docker/install-graal.sh
@@ -4,4 +4,8 @@ source ~/.sdkman/bin/sdkman-init.sh
 VERSION=$(sdk ls java | grep grl | head -1 | xargs -n 1 echo | tail -1)
 yes | sdk i java $VERSION
 
+VERSION=$(ls -1 ~/.sdkman/candidates/java | grep -v current)
+export JAVA_HOME=~/.sdkman/candidates/java/$VERSION
+export PATH=$PATH:$JAVA_HOME/bin
+
 gu install native-image
