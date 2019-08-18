8dbdb2e4ae809fa6d8eb708d1198f7dcbb238bac
Rahul Somasunderam <rahul.som@gmail.com>
2019-08-17 21:57:11 -0700

    Fix computation of latest graal version

--- .docker/install-graal.sh
+++ .docker/install-graal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ~/.sdkman/bin/sdkman-init.sh
-VERSION=$(sdk ls java | xargs -n 1 echo | grep grl | head -1)
+VERSION=$(sdk ls java | grep grl | head -1 | xargs -n 1 echo | tail -1)
 yes | sdk i java $VERSION
